0ab5713c8a3875ad31445c27dd960fcc2d233bce
Matheus Marques <matheusm537@gmail.com>
2021-11-03 09:41:14 -0300

    Init project database connection and migrations

---
+++ backend/src/database/migrations/03_create_schedules.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex){
+  return knex.schema.createTable('schedules', table => {
+    table.increments('id').primary();
+    table.integer('user_id').notNullable().references('id').inTable('user')
+    table.dateTime('date').notNullable();
+    table.string('local');
+    table.string('description');
+  })
+};
+
+export async function down(knex: Knex){
+  return knex.schema.dropTable('schedules');
+}
