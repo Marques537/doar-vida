13f2e835fb7ec138a79a1d5eb8110f77d93a34ad
Matheus Marques <matheusm537@gmail.com>
2021-11-15 23:27:56 -0300

    Create new routes and fix migration of schedule

--- backend/src/database/migrations/03_create_schedules.ts
+++ backend/src/database/migrations/03_create_schedules.ts
@@ -3,10 +3,9 @@ import { Knex } from 'knex';
 export async function up(knex: Knex){
   return knex.schema.createTable('schedules', table => {
     table.increments('id').primary();
-    table.integer('user_id').notNullable().references('id').inTable('user');
+    table.integer('user_id').notNullable().references('id').inTable('users');
     table.integer('point_id').notNullable().references('id').inTable('points');
     table.dateTime('date').notNullable();
-    table.string('local');
     table.string('description');
   })
 };
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,14 @@
 import express, { request, response } from 'express';
-import knex from './database/connection';
 import PointsController from './controllers/PointsController';
+import ScheduleController from './controllers/ScheculesController';
+import UserController from './controllers/UserController';
+import DonationController from './controllers/DonationController';
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const scheduleController = new ScheduleController();
+const userController = new UserController();
+const donationController = new DonationController();
 
 routes.get('/', (request, response) => {
   return response.json({
@@ -14,5 +19,11 @@ routes.get('/', (request, response) => {
 routes.get('/points/:id', pointsController.show);
 routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
+routes.post('/schedule',scheduleController.create);
+routes.get('/schedule/:user_id',scheduleController.showAll);
+routes.get('/schedules',scheduleController.showByDate);
+routes.post('/user', userController.create);
+routes.post('/donation', donationController.create);
+routes.get('/donations/:user_id', donationController.show);
 
 export default routes;
